c22fb9a0e68e921d3e21e624ccbc035c9ded866e
dosa <almazbekovd12@gmail.com>
2025-06-23 11:37:08 +0600

    edit logofragment

--- src/components/LogoFragment/LogoFragment.jsx
+++ src/components/LogoFragment/LogoFragment.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import styles from "./LogoFragment.module.css"
-import logo from '../../assets/auth/logo.svg'
+import logo from '../../assets/logo.svg'
 
 const LogoFragment = () => {
   return (
-    <div className={styles.logoFragmentWrap}> <img src={logo} alt="" /></div>
+    <div className={styles.logoFragmentWrap}>
+      <img src={logo} alt="" />
+      <h1>InternFlow</h1>
+    </div>
   )
 }
 
